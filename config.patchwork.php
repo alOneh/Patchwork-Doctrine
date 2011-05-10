122b73c07b165f6108e85a0637e8306005f2b096
Hippolyte Alain <alain.hippolyte@gmail.com>
2011-05-10 14:33:48 +0200

    + Add missing file

---
+++ config.patchwork.php
@@ -0,0 +1,11 @@
+<?php
+
+$CONFIG += array(
+    'DSN' => array(
+        'dbname'   => 'database',
+        'user'     => 'user',
+        'password' => 'password',
+        'host'     => 'localhost',
+        'driver'   => 'pdo_mysql'
+    )
+);
