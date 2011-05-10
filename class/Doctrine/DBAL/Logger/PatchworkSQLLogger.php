03761c8efdd69dce995d7850ed7098bbe2c080fd
nicolas.grekas <nicolas.grekas@intellagence.eu>
2011-05-10 14:52:21 +0200

    * cleanups

--- class/Doctrine/DBAL/Logger/PatchworkSQLLogger.php
+++ class/Doctrine/DBAL/Logger/PatchworkSQLLogger.php
@@ -5,17 +5,23 @@ namespace Doctrine\DBAL\Logger;
 use Doctrine\DBAL\Logging\SQLLogger;
 
 /**
- * A SQL logger that logs to the Patchwork debug bar, Doctrine query.
+ * A SQL logger that logs queries and parameters to the Patchwork debug window.
  * 
  */
 class PatchworkSQLLogger implements SQLLogger
 {
+    protected $query;
+
     /**
      * {@inheritdoc}
      */
     public function startQuery($sql, array $params = null, array $types = null)
     {
-        E($sql, $params);
+        E(array(
+            'sql'    => $sql,
+            'params' => $params,
+            'types'  => $types
+        ));
     }
 
     /**
@@ -23,5 +29,6 @@ class PatchworkSQLLogger implements SQLLogger
      */
     public function stopQuery()
     {
+        E('End of query');
     }
 }
