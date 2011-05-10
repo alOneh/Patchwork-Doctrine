99208d8ad39f0110fde295c0946bf9e237e1c04e
Hippolyte Alain <alain.hippolyte@gmail.com>
2011-05-10 15:41:51 +0200

    + Add copyright

--- class/loop/sql/Doctrine.php
+++ class/loop/sql/Doctrine.php
@@ -1,4 +1,16 @@
-<?php
+<?php /****************** vi: set fenc=utf-8 ts=4 sw=4 et: *****************
+ *
+ *   Copyright : (C) 2011 Nicolas Grekas. All rights reserved.
+ *   Email     : p@tchwork.org
+ *   License   : http://www.gnu.org/licenses/agpl.txt GNU/AGPL
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ ***************************************************************************/
+
 
 class loop_sql_Doctrine extends loop
 {
