0eeeda3e2035f56a6f10fbc04eacdfe1c76dd39b
Hippolyte Alain <alain.hippolyte@gmail.com>
2011-08-30 16:02:21 +0200

    * Fix delete config.patchwork.php, it is required

--- config.patchwork.php
+++ config.patchwork.php
@@ -1,19 +1,21 @@
 <?php
 
 $CONFIG += array(
+/*
     'DSN' => array(
         'dbname'   => 'database',
         'user'     => 'user',
         'password' => 'password',
         'host'     => 'localhost',
         'driver'   => 'pdo_mysql'
-    ),
+			),
+*/
     'doctrine.cache'       => '\Doctrine\Common\Cache\ArrayCache', // use ApcCache for production env
     'doctrine.mapping.dir' => 'data/mapping',
     'doctrine.proxy.dir'   => 'class/Proxies',
-    'doctrine.proxy.generate' => true, // set to false to production env
-    'doctrine.dbal.logger'      => '',
-    'doctrine.event'       => false,
+    'doctrine.proxy.generate'  => true, // set to false to production env
+    'doctrine.dbal.logger'     => '',
+    'doctrine.event'           => false,
     'doctrine.event.listeners' => array(),
 );
 
